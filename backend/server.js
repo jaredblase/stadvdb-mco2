7ab07b5fe0ef16af49c366c0df382425cead73d0
Jared Sy <jaredblase@gmail.com>
2022-02-14 19:31:17 +0800

    remove unused server in backend

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,15 @@ import express from 'express'
 import cors from 'cors'
 import router from './routes.js'
 
-const app1 = express()
-const app2 = express()
+const app = express()
 
-app1.set('port', 3001)
-app2.set('port', 3002)
+app.set('port', 3001)
 
-const servers = [app1, app2]
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors({ origin: 'http://localhost:3000' }))
 
-servers.forEach(app => {
-  app.use(express.urlencoded({ extended: true }))
-  app.use(express.json())
-  app.use(cors({ origin: 'http://localhost:3000' }))
+// routes
+app.use('/api/movies', router)
 
-  // routes
-  app.use('/api/movies', router)
-})
-
-servers.forEach(app => {
-  app.listen(app.get('port'), () => console.log(`Find the server at: http://localhost:${app.get('port')}/`))
-})
+app.listen(app.get('port'), () => console.log(`Find the server at: http://localhost:${app.get('port')}/`))
