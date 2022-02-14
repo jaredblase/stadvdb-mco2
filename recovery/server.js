950bf1fecfb93e9b8b7b347cf3fbd52cf2604709
Jared Sy <jaredblase@gmail.com>
2022-02-14 20:34:10 +0800

    use setInterval

--- recovery/server.js
+++ recovery/server.js
@@ -1,19 +1,13 @@
 import express from 'express'
-import cors from 'cors'
+import replicate from './replicate.js'
 
 const app = express()
 
-app.set('port', 3003)
+app.set('port', 3002)
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors({ origin: 'http://localhost:3000' }))
 
-// routes
-// app.use('/api/recovery', router)
-
-setInverval(() => {
-    console.log('hi')
-}, 5000)
+setInterval(replicate, 1000)
 
 app.listen(app.get('port'), () => console.log(`Find the server at: http://localhost:${app.get('port')}/`))
