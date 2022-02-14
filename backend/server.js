8a0a6d5629c03341004f6d4cdda459005d4f4880
Jared Sy <jaredblase@gmail.com>
2022-02-14 20:36:48 +0800

    fix: heroku config

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import router from './routes.js'
+import 'dotenv/config'
 
 const app = express()
 
-app.set('port', 3001)
+app.set('port', process.env.PORT || 3001)
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(cors({ origin: process.env.NODE_ENV == 'production' ? process.env.PRODUCTION_DOMAIN : 'http://localhost:3000' }))
 
 // routes
 app.use('/api/movies', router)
