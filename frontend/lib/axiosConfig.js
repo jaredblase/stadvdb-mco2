79ab270f67ac775eb84a92be3bc42afe9627710a
Jared Sy <jaredblase@gmail.com>
2022-02-14 15:40:49 +0800

    update env

--- frontend/lib/axiosConfig.js
+++ frontend/lib/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const app = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     'Access-Control-Allow-Credentials': 'true',
   },
