0fa0d64938f49b59ebb79b50aeb59acfa4a5ddb4
GabPua <gabriel_pua@dlsu.edu.ph>
2022-02-16 00:12:06 +0800

    add: added more queries to report.js

--- backend/report.js
+++ backend/report.js
@@ -1,12 +1,20 @@
 import { db2, db3, query1, query2, query3 } from './lib/dbs.js'
 import promiseHandler from './lib/promiseHandler.js'
+import genres from './lib/genres.js'
 
 const queries = [
-    'SELECT COUNT(*) FROM movies',
-    'SELECT COUNT(*), SUM(`rank`) FROM movies WHERE `rank` IS NOT NULL',
-    'SELECT COUNT(*) FROM movies GROUP BY `year` DIV 10',
-    'SELECT COUNT(*), SUM(`rank`) FROM movies WHERE `rank` IS NOT NULL;'
+    'SELECT COUNT(*) AS cnt FROM movies',
+    'SELECT COUNT(*) AS cnt, SUM(`rank`) AS val FROM movies WHERE `rank` IS NOT NULL',
+    'SELECT COUNT(*) AS cnt, `year` DIV 10 * 10 AS decade FROM movies GROUP BY `year` DIV 10',
+    'SELECT COUNT(*) AS cnt, SUM(`rank`) AS val, `year` DIV 10 * 10 AS decade FROM movies' + 
+        ' WHERE `rank` IS NOT NULL GROUP BY `year` DIV 10 ORDER BY decade ASC'
 ]
+
+genres.forEach((genre) => {
+    queries.push(`SELECT COUNT(*) AS cnt, '${genre}' AS genre FROM movies WHERE genre1 = '${genre}' OR genre2 = '${genre}'`)
+    queries.push(`SELECT COUNT(*) AS cnt, SUM(\`rank\`) AS val, '${genre}' AS genre FROM movies WHERE \`rank\` IS NOT NULL AND 
+        (genre1 = '${genre}' OR genre2 = '${genre}')`)
+})
 
 const queryString = queries.join(';\n')
 
@@ -16,7 +24,7 @@ export default async function getReport(req, res) {
 
         const results = await query1(queryString, [], 'READ')
         console.log(results)
-        res.json({ result: true })
+        res.json({ results })
     } catch (e) {
         console.log(e)
 
@@ -24,6 +32,26 @@ export default async function getReport(req, res) {
         const [ { data: data1 }, { data: data2 }] = await Promise.all(tasks.map(promiseHandler))
         console.log(data1)
         console.log(data2)
-        res.json({ result: true })
+
+        let results = null
+        if (data1 && data2) {
+            results = data1
+            for (let i = 0; i < 46; i++) {
+                if (i < 2 || i > 3) {
+                    results[i].cnt += data2[i].cnt
+                    if (i % 2 === 1) {
+                        results[i].val += data2[i].val
+                    }
+                } else {
+                    results[i].push(data2[i])
+                }
+            }
+        } else if (data1) {
+            results = data1
+        } else if (data2) {
+            results = data2
+        }
+
+        res.json({ results })
     }
 }
